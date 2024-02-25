4082bfab141aef7d7242cc3fcb8245ca68e292df
qq3173732005 <76537620+qq3173732005@users.noreply.github.com>
2024-02-25 05:00:41 -0500

    comments for clarity

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, redirect
 
+# create app
 app = Flask(__name__)
 
+# home page redirects to login page
 @app.route('/')
 def index():
     return redirect('/login', code=301)
 
+# login page
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+# run app
 if __name__ == '__main__':
     app.run()
