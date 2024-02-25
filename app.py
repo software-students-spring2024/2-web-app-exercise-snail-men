11b55679be96380d909f2c79e6a59f71e7c1f5d0
qq3173732005 <76537620+qq3173732005@users.noreply.github.com>
2024-02-25 14:21:54 -0500

    routes to pages

--- app.py
+++ app.py
@@ -13,6 +13,31 @@ def index():
 def login():
     return render_template('login.html')
 
+# profile page
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+# picture history page
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
+# account creation page
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+# picture change page
+@app.route('/change-pfp')
+def signup():
+    return render_template('change-pfp.html')
+
+# account deletion page
+@app.route('/delete-account')
+def signup():
+    return render_template('delete-account.html')
+
 # run app
 if __name__ == '__main__':
     app.run()
