231e27e2246f1d61d9d6baea3296436b2296b296
qq3173732005 <76537620+qq3173732005@users.noreply.github.com>
2024-02-25 04:40:52 -0500

    redirect home page to login page

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect('/login', code=301)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
